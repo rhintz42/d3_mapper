feb40af825fcead802b3df7f35e67b05464462e5
Robert Hintz <roberth@surveymonkey.com>
2014-04-17 15:06:24 -0700

    Add a couple more things like the logging functions and other d3 thing

--- d3_mapper/__init__.py
+++ d3_mapper/__init__.py
@@ -7,6 +7,8 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home3', '/home3')
+    config.add_route('home2', '/home2')
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
--- d3_mapper/views.py
+++ d3_mapper/views.py
@@ -4,3 +4,11 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/index.pt')
 def my_view(request):
     return {'project': 'd3_mapper'}
+
+@view_config(route_name='home2', renderer='templates/index_2.pt')
+def my_view2(request):
+    return {'project': 'd3_mapper'}
+
+@view_config(route_name='home3', renderer='templates/logging_functions.pt')
+def my_view3(request):
+    return {'project': 'd3_mapper'}
